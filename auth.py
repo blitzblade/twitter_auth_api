e29932e84e15f7c3afca199fbb30ca1c5ad2bb77
Kwesi Dadson <samson.dadson@gmail.com>
2021-06-13 19:11:50 +0000

    allow passing of api keys

--- auth.py
+++ auth.py
@@ -29,13 +29,15 @@ def authorize_twitter():
     print("callback url: ",callback_url)
     print('CURRENT DIRECTORY: ', current_dir)
     config = get_config(f"{current_dir}/config.json",'kwesi_dadson')
+    consumer_key = request.args.get('consumer_key') if request.args.get('consumer_key') else config["consumer_key"]
+    consumer_secret = request.args.get('consumer_secret') if request.args.get('consumer_secret') else config["consumer_secret"]
 
-    auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"], callback_url)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
 
     try:
         redirect_url = auth.get_authorization_url()
         print("REDIRECT: ", redirect_url)
-        return json.dumps({"message":"success", "redirect_url": redirect_url})
+        return json.dumps({"message":"success", "redirect_url": redirect_url })
     except tweepy.TweepError as ex:
         print(ex)
         print('Error! Failed to get request token.')
@@ -47,12 +49,17 @@ def twitter_callback():
     try:
         oauth_token = request.args.get('oauth_token')
         oauth_verifier = request.args.get('oauth_verifier')
+
+
         authorizer = "kwesi_dadson"
         print("OAUTH TOKEN: ", oauth_token)
         print("OAUTH VERIFIER: ", oauth_verifier)
-
         config = get_config(f"{current_dir}/config.json",authorizer)
-        auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
+
+        consumer_key = request.args.get('consumer_key') if request.args.get('consumer_key') else config["consumer_key"]
+        consumer_secret = request.args.get('consumer_secret') if request.args.get('consumer_secret') else config["consumer_secret"]
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
         auth.request_token = { "oauth_token": oauth_token, "oauth_token_secret": oauth_verifier }
         auth.get_access_token(oauth_verifier)
